46856b48387b841b73a6bf47d764949ccfa72afc
wangyanchao <wangyanchao@me.com>
2015-06-03 17:30:15 +0800

    cm sequential

---
+++ AnimationDemo/WWSequential.h
@@ -0,0 +1,13 @@
+//
+//  WWSequential.h
+//  AnimationDemo
+//
+//  Created by wangyanchao on 15/6/3.
+//  Copyright (c) 2015年 wangyanchao. All rights reserved.
+//
+
+#import "WWEffect.h"
+
+@interface WWSequential : WWEffect
+
+@end
